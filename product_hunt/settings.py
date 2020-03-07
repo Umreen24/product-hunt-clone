ff433861d3dd7cb8a8962556fc8c52caffea4389
Umreen <uidigitalcrafts@gmail.com>
2020-03-06 23:35:42 -0600

    created signup pages

--- product_hunt/settings.py
+++ product_hunt/settings.py
@@ -128,4 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'product_hunt/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
